c34914b78edee5dc1d58bfa28e3f7be6779bfc69
JohnnyMacaronee <sergotashit@gmail.com>
2022-05-25 21:20:05 +0300

    updated main

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,8 +1,17 @@
+import pathlib
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+BASE_DIR = pathlib.Path(__file__).parent
+ROOT_PROJECT_DIR = BASE_DIR.parent
+TEMPLATE_DIR = ROOT_PROJECT_DIR / "html"  # /var/www/html
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory=str(TEMPLATE_DIR))
 
-@app.get("/")
+
+@app.get("/", response_class=HTMLResponse)
 def home():
-    return {"home": "page"}
+    return templates.TemplateResponse("index.nginx-debian.html", {})
