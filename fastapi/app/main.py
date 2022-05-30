66f25d0b2f0bd14b46c1ce7d1e7432fee36e0960
JohnnyMacaronee <sergotashit@gmail.com>
2022-05-30 06:17:16 +0300

    everything should work now, template dir lead to wrong dir

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -5,11 +5,10 @@ from fastapi.templating import Jinja2Templates
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 ROOT_PROJECT_DIR = BASE_DIR.parent
-TEMPLATE_DIR = ROOT_PROJECT_DIR / "html" # /var/www/html/
+TEMPLATE_DIR = ROOT_PROJECT_DIR.parent / "html" # /var/www/html/
 app = FastAPI()
 
-templates = Jinja2Templates(directory=('/home/kali/dev/python-server/html'))
-
+templates = Jinja2Templates(directory=str(TEMPLATE_DIR))
 
 @app.get("/", response_class=HTMLResponse) # html -> localhost:8000/
 def home(request:Request):
