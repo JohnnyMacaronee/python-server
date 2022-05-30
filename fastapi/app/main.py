49485c9dcf8a83bf8279dc3a9d76f010bb0f2aa1
JohnnyMacaronee <sergotashit@gmail.com>
2022-05-30 06:07:26 +0300

    works on localhost, doesnt work on server, err500

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,17 +1,16 @@
 import pathlib
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-BASE_DIR = pathlib.Path(__file__).parent
+BASE_DIR = pathlib.Path(__file__).resolve().parent
 ROOT_PROJECT_DIR = BASE_DIR.parent
-TEMPLATE_DIR = ROOT_PROJECT_DIR / "html"  # /var/www/html
-
+TEMPLATE_DIR = ROOT_PROJECT_DIR / "html" # /var/www/html/
 app = FastAPI()
 
-templates = Jinja2Templates(directory=str(TEMPLATE_DIR))
+templates = Jinja2Templates(directory=('/home/kali/dev/python-server/html'))
 
 
-@app.get("/", response_class=HTMLResponse)
-def home():
-    return templates.TemplateResponse("index.nginx-debian.html", {})
+@app.get("/", response_class=HTMLResponse) # html -> localhost:8000/
+def home(request:Request):
+    return templates.TemplateResponse("index.nginx-debian.html", {"request": request, "title": "Jinja"})
