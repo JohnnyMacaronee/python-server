d062ab28ca4c7b151ab6d1ef2da9ac27912d55a2
JohnnyMacaronee <sergotashit@gmail.com>
2022-06-01 18:44:56 +0300

    added schema and testing

---
+++ fastapi/app/schema.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from pydantic import BaseModel as BaseModelSchema, validator
+import datetime
+
+class EntryCreateSchema(BaseModelSchema):
+    title: str
+    content: Optional[str] = None
+
+    @validator("title")
+    def title_length(cls, value, **kwargs):
+        if len(value) > 100:
+            raise ValueError(f"{value} cannot exceed 100")
+        return value
+
+
+class EntryListSchema(EntryCreateSchema):
+    id: int
+    timestamp: Optional[datetime.datetime] = datetime.datetime.utcnow()
+    updated: Optional[datetime.datetime] = datetime.datetime.utcnow()
+
+"""
+id
+title
+content
+timestamp
+updated
+"""
